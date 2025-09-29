47f3eaa217fa26f75466b59a6b62ef8b9be8243d
William Horn <wbhorn@alaska.edu>
2025-09-29 13:21:41 -0800

    Add test for get_granules

--- tests/test_chip_sentinel1rtc.py
+++ tests/test_chip_sentinel1rtc.py
@@ -1,4 +1,5 @@
 import datetime
+from unittest.mock import MagicMock, patch
 
 import pytest
 
@@ -15,11 +16,24 @@ def test_bounds_check():
 
 
 def test_get_granules():
-    start_date = datetime.datetime(2020, 7, 7)
-    end_date = start_date + datetime.timedelta(days=14)
+    bounds = [-107.79192, 45.74287, -105.01543, 46.48598]
+    date_start = datetime.datetime(2020, 7, 7)
+    date_end = date_start + datetime.timedelta(days=14)
 
-    assert chip_sentinel1rtc.get_granules([-107.79192, 45.74287, -105.01543, 46.48598], start_date, end_date)
+    mock_search_result = MagicMock()
+    mock_search_result.items = ['granule1', 'granule2']
 
+    with patch('satchip.chip_sentinel1rtc.asf.geo_search', return_value=mock_search_result) as mock_geo_search:
+        results = chip_sentinel1rtc.get_granules(bounds, date_start, date_end)
 
-def test_pair_slcs_to_chips():
-    pass
+        mock_geo_search.assert_called_once()
+
+        assert results == mock_search_result
+
+        args, kwargs = mock_geo_search.call_args
+        assert (
+            kwargs['intersectsWith']
+            == 'POLYGON ((-105.01543 45.74287, -105.01543 46.48598, -107.79192 46.48598, -107.79192 45.74287, -105.01543 45.74287))'
+        )
+        assert kwargs['start'] == date_start
+        assert kwargs['end'] == date_end + datetime.timedelta(days=1)
