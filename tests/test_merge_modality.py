0169313ae3e0bcc49bad0eaae98bd51afd75d95d
William Horn <wbhorn@alaska.edu>
2026-04-01 11:03:35 -0800

    Add better tests for merge modality

--- tests/test_merge_modality.py
+++ tests/test_merge_modality.py
@@ -1,4 +1,5 @@
 from satchip import merge_modality, models
+import rasterio
 
 
 def test_make_merge_name(s2_event):
@@ -21,11 +22,23 @@ def test_merge_s2_modality(s2_local_files, s2_event, tmp_path):
     assert len(merged_files) == len(models.HLS_S30['bands'])
     assert all('merged' in str(result) for result in merged_files)
 
+    shapes = set()
+    for merged_file in merged_files:
+        with rasterio.open(merged_file) as ds:
+            shapes.add(ds.shape)
+
+    assert len(shapes) == 1
+
 
 def test_stack_bands(s2_local_files, s2_event, tmp_path):
-    bands = tuple(b for b in models.HLS_S30['bands'] if b.shortname != 'fmask')
+    bands = tuple(b for b in models.HLS_S30['bands'] if b.id != 'Fmask')
 
     merged_bands = merge_modality.merge_modality(s2_local_files, models.HLS_S30, s2_event, tmp_path / 'merged', bands)
     stacked_data = merge_modality.stack_bands(merged_bands, stacked_filename=tmp_path / 'stacked.tif')
 
     assert 'stacked.tif' in stacked_data.name
+
+    with rasterio.open(stacked_data) as ds:
+        num_bands = ds.count
+
+    assert num_bands == len(bands)
